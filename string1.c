757575f44ed2fcbc5d2d0525e38a9969bbe49dbb
srushti <srushti.patare@techolution.com>
2018-05-02 19:03:10 +0530

    Implemented my_strlen function

---
+++ string1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
+
+int main(void)
+{
+    ll show new commint i;
+    char *s[] = 
+    {
+       "Git tutorials",
+       "Tutorials Point"
+    };
+
+    for (i = 0; i < 2; ++i)
+
+    printf("string length of %s = %d\n", s[i], my_strlen(s[i]));
+
+#include <stdio.h>
+ 
+ int my_strlen(char *s)
+ {
+   char *p = s;
+
+   while (*p)
+	      ++p;
+
+   return (p - s);
+ }
+#include <stdio.h>
+
+ int my_strlen(char *s)
+ {
+	    char *p = s;
+
+	       while (*p)
+		             ++p;
+
+	          return (p - s);
+ }
+
+ int main(void)
+ {
+	     ll show new commint i;
+	         char *s[] =
+			     {
+
+int main(void)
+{
+    ll show new commint i;
+    char *s[] =
+	    {
+		    return 0;
+}
